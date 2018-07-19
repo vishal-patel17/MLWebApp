aaab9b475657bcd8f8a572d4dd5a5a8e6ed436b5
Patel <vp185104@ncr.com>
2018-07-19 15:17:13 +0530

    Added Home Page

--- app.py
+++ app.py
@@ -8,8 +8,14 @@ app = Flask(__name__)
 app.secret_key = 'random string'
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/rfr.html')
 def index():
-    return render_template('index.html')
+    return render_template('rfr.html')
+
 
 @app.route('/import', methods = ['GET', 'POST'])
 def upload_file():
@@ -19,14 +25,16 @@ def upload_file():
 
       upload_file.dataSet = pd.read_csv(f.filename)
       flash('Imported '+ f.filename+ ' successfully')
-      return render_template('index.html', dataSet = upload_file.dataSet)
+      return render_template('rfr.html', dataSet = upload_file.dataSet)
+
 
 @app.route('/divide', methods=['GET', 'POST'])
 def func():
     func.X = upload_file.dataSet.iloc[:, 1:2].values
     func.Y = upload_file.dataSet.iloc[:, 2].values
     flash('Divided into Dependent and Independent variables.')
-    return render_template('index.html', dataSet = upload_file.dataSet)
+    return render_template('rfr.html', dataSet = upload_file.dataSet)
+
 
 @app.route('/rfc', methods=['Get', 'POST'])
 def rfc():
@@ -34,7 +42,8 @@ def rfc():
     regressor = RandomForestRegressor(n_estimators=10, random_state=0)
     regressor.fit(func.X, func.Y)
     y_pred = regressor.predict(yoe)
-    return render_template('index.html', dataSet = upload_file.dataSet, prediction=y_pred)
+    return render_template('rfr.html', dataSet = upload_file.dataSet, prediction=y_pred)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
